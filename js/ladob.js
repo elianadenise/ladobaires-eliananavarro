ad0b7bcb74580235311061385bfdd673ee733801
Eliana Navarro <elianadnavarro@gmail.com>
2022-02-27 21:17:56 -0300

    primer desafio

--- js/ladob.js
+++ js/ladob.js
@@ -8,42 +8,35 @@
 
 let cityTour = prompt("Ingrese el nombre del City Tour elegido");
 let pax = parseInt(prompt("Ingrese el numero de pasajeros"))
-let precio1 = 1925;
+let precio1 = 2499;
 let precio2 = 3499;
 
-function cotizacion(){
-    switch(cityTour != ""){
-        case "misteriosa buenos aires":
-            let resultado = precio1*pax;
-            alert(`la cotización final por ${pax} es de $ ${resultado}`);
-            break
-        case "noche de tango":
-            let resultado = precio2*pax;
-            alert(`la cotización final por ${pax} es de $ ${resultado}`);
-            break
-        case "arquitectura porteña":
-            let resultado = precio2*pax;
-            alert(`la cotización final por ${pax} es de $ ${resultado}`);
-            break
-        case "arte urbano":
-            let resultado = precio2*pax;
-            alert(`la cotización final por ${pax} es de $ ${resultado}`);
-            break
-        case "tango en la calle":
-            let resultado = precio1*pax;
-            alert(`la cotización final por ${pax} es de $ ${resultado}`);
-            break
-        case "subterraneo":
-            let resultado = precio2*pax;
-            alert(`la cotización final por ${pax} es de $ ${resultado}`);
-            break
-        default:
-            alert(`no existe el City Tour ingresado`);
-            cityTour;
+function cotizacion(cityTour, pax){
+    if (cityTour = "misteriosa buenos aires"){
+        let resultado = precio1*pax;
+        alert(`la cotización final por ${pax} es de $ ${resultado}`);
+    }else if(cityTour = "noche de tango"){
+        let resultado = precio2*pax;
+        alert(`la cotización final por ${pax} es de $ ${resultado}`);
+    }else if(cityTour = "arquitectura porteña"){
+        let resultado = precio2*pax;
+        alert(`la cotización final por ${pax} es de $ ${resultado}`);
+    }else if(cityTour = "arte urbano"){
+        let resultado = precio2*pax;
+        alert(`la cotización final por ${pax} es de $ ${resultado}`);
+    }else if(cityTour = "tango en la calle"){
+        let resultado = precio2*pax;
+        alert(`la cotización final por ${pax} es de $ ${resultado}`);
+    }else if(cityTour = "subterraneo"){
+        let resultado = precio1*pax;
+        alert(`la cotización final por ${pax} es de $ ${resultado}`);
+    }else if(cityTour = ""){
+        alert(`El City Tour ingresado no es válido`);
     }
 
     return cotizacionFinal
 
 }
 
-let precioFinal = cotizacion("misteriosa buenos aires", 2);
+let precioFinal = cotizacion(cityTour, pax);
+alert(precioFinal);
